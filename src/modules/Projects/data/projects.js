90682bb4743062bdd3700180523ee4e96a3e44c2
Gabriel Duarte <gabriel.duartepaz@gmail.com>
2022-11-15 15:29:40 -0300

    bugfix: Ajusta título da página e atualiza informações do portfólio.

--- src/modules/Projects/data/projects.js
+++ src/modules/Projects/data/projects.js
@@ -1,10 +1,17 @@
 export const projectsData = [
     {
-        id: 3,
-        title: 'Pokedex',
-        description: "For those who don't know, a pokedex is an encyclopedia used to register as the different pokemons you find in your journey. In this project I listed all the existing pokemons using the PokeAPI. The code contemplate only the front-end and was done using React, with React Router Dom, Styled Components and some other popular dependencies.",
-        tags: ['HTML5', 'CSS3', 'SASS', 'JavaScript ES6+', 'React'],
-        link: "https://laughing-darwin-58affe.netlify.app"
+        id: 1,
+        title: 'Activities Register - FrontEnd',
+        description: "This project was developed to study more about Preact and how it can improve application performance using the same React paradigms. In this app you can register users and associate then with activies, to keep track of their actions.",
+        tags: ['HTML5', 'CSS3', 'TypeScript', 'Preact', 'Vite'],
+        link: "https://activities-front.vercel.app/"
+    },
+    {
+        id: 2,
+        title: 'Activities Register - BackEnd',
+        description: "This project was created to reinforce the studies along with the freeCodeCamp, so I decided to create my first REST API with NodeJS, Express and Mongoose. In this app you can register users and associate then with activies, to keep track of their actions.",
+        tags: ['NodeJS', 'Express', 'MongoDB', 'Heroku'],
+        link: "https://activities-restapi.herokuapp.com/"
     },
     {
         id: 3,
@@ -14,14 +21,21 @@ export const projectsData = [
         link: "https://chatbot-consultorio-medico.glitch.me"
     },
     {
-        id: 3,
-        title: 'SaveLinks - Coming Soon!',
-        description: "This project is being built right now. I won't will describe the idea here entirely  so i don't give you any spoilers, but this is my most ambitious project so far. The code is fullstack and uses Next.js, Express and other popular libs at the moment. Come here soon to check it out.",
-        tags: ['HTML5', 'SASS', 'TypeScript', 'Next.js', 'Node.js', 'Express'],
-        link: "#"
-    },
-    {
         id: 4,
+        title: 'Pokedex',
+        description: "For those who don't know, a pokedex is an encyclopedia used to register as the different pokemons you find in your journey. In this project I listed all the existing pokemons using the PokeAPI. The code contemplate only the front-end and was done using React, with React Router Dom, Styled Components and some other popular dependencies.",
+        tags: ['HTML5', 'CSS3', 'SASS', 'JavaScript ES6+', 'React'],
+        link: "https://laughing-darwin-58affe.netlify.app"
+    },
+    // {
+    //     id: 3,
+    //     title: 'SaveLinks - Coming Soon!',
+    //     description: "This project is being built right now. I won't will describe the idea here entirely  so i don't give you any spoilers, but this is my most ambitious project so far. The code is fullstack and uses Next.js, Express and other popular libs at the moment. Come here soon to check it out.",
+    //     tags: ['HTML5', 'SASS', 'TypeScript', 'Next.js', 'Node.js', 'Express'],
+    //     link: "#"
+    // },
+    {
+        id: 5,
         title: 'GitHub Projects',
         description: 'If you wanna know more about my projects, please visit my GitHub profile, there you will see many other repositories and can even verify my consistency on trying to do code every single day to improve my knowlage.',
         tags: ['Python', 'Java', 'Jupyter Notebook'],
